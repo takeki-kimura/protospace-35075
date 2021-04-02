0c2919e9c4cd423accaf9e2a7effdd0ee8501752
木村毅樹 <80314997+takeki-kimura@users.noreply.github.com>
2021-04-02 12:55:16 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    if comment.save
-       redirect_to "/prototypes/#{comment.prototype.id}"
+    @comment = Comment.create(comment_params)
+    if @comment.save
+       redirect_to "/prototypes/#{@comment.prototype.id}"
     else
-      render "/prototypes/show"
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 
